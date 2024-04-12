a3772aed9cce4a476f85260119ae9f99de35c916
marcheanin <marcheanin1576@gmail.com>
2024-04-12 21:01:06 +0300

    fix 6th lab

--- lab6_cpp/main.cpp
+++ lab6_cpp/main.cpp
@@ -26,12 +26,31 @@ double df_dydy(double x, double y) {
     return 4 - (60 * x * x * x * y) / ((10 + 10 * x * x * y * y) * (10 + 10 * x * x * y * y));
 }
 
+double fi_first(double x, double y) {
+    return -std::pow(df_dx(x, y), 2) - std::pow(df_dy(x,y), 2);
+}
+
+double fi_second(double x, double y) {
+    return df_dxdx(x, y) + std::pow(df_dx(x, y), 2) +
+                    2 * df_dxdy(x, y) * df_dx(x, y) * df_dy(x, y) +
+                    df_dydy(x, y) * std::pow(df_dy(x, y), 2);
+}
+
 int main() {
-    double eps = 0.001;
+    double eps = 0.005;
     double x_k = -1, y_k = 0; // начальные значения
     double x_check = -1.011, y_check = 0.07;
-
-    while (std::max(std::abs(df_dx(x_k, y_k)), std::abs(df_dy(x_k, y_k))) > eps) {
-        
+    double fi1, fi2, t, error = 100;
+    while (error > eps) {
+        fi1 = fi_first(x_k, y_k);
+        fi2 = fi_second(x_k, y_k);
+        t = - fi1 / fi2;
+        x_k -= t * df_dx(x_k, y_k);
+        y_k -= t * df_dy(x_k, y_k);
+        error = std::max(std::abs(df_dx(x_k, y_k)), std::abs(df_dy(x_k, y_k)));
+        std::cout << x_k << " " << y_k  << " " << error << std::endl;
     }
+    std::cout << x_k << " " << y_k << std::endl;
+    std::cout << x_check << " " << y_check;
+    return 0;
 }
